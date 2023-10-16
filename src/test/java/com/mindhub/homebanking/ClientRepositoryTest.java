913eaf11cb13920d66094837e115a82040c44370
iacastillo90 <137194587+iacastillo90@users.noreply.github.com>
2023-10-15 21:53:02 -0300

    Ultimos retoques

--- src/test/java/com/mindhub/homebanking/ClientRepositoryTest.java
+++ src/test/java/com/mindhub/homebanking/ClientRepositoryTest.java
@@ -1,23 +1,36 @@
 package com.mindhub.homebanking;
-
-import com.mindhub.homebanking.models.Client;
-import com.mindhub.homebanking.repositories.ClientRepository;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.context.annotation.Import;
-
-import java.util.List;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
-import static org.hamcrest.Matchers.is;
-
-
-@DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-public class ClientRepositoryTest {
-
-}
+//
+//import com.mindhub.homebanking.models.Client;
+//import com.mindhub.homebanking.repositories.ClientRepository;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+//import org.springframework.context.annotation.Import;
+//
+//import java.util.List;
+//
+//import static org.hamcrest.MatcherAssert.assertThat;
+//import static org.hamcrest.Matchers.*;
+//import static org.hamcrest.Matchers.is;
+//
+//
+//@DataJpaTest
+//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+//public class ClientRepositoryTest {
+//    @Autowired
+//    private ClientRepository clientRepository;
+//
+//    @Test
+//    public void existClients(){
+//        List<Client> clients = clientRepository.findAll();
+//        assertThat(clients,is(not(empty())));
+//    }
+//
+//    @Test
+//    public void existClientsByEmail(){
+//        List<Client> clients = clientRepository.findAll();
+//        assertThat(clients, hasItem(hasProperty("email", is("melba@mindhub.com"))));
+//    }
+//}
 
--- src/test/java/com/mindhub/homebanking/TransactionRepositoryTest.java
+++ src/test/java/com/mindhub/homebanking/TransactionRepositoryTest.java
@@ -1,19 +1,26 @@
-package com.mindhub.homebanking;
-
-import com.mindhub.homebanking.models.Transaction;
-import com.mindhub.homebanking.models.TransactionType;
-import com.mindhub.homebanking.repositories.TransactionRepository;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-
-import java.util.List;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
-@DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-public class TransactionRepositoryTest {
-
-}
+//package com.mindhub.homebanking;
+//
+//import com.mindhub.homebanking.models.Transaction;
+//import com.mindhub.homebanking.models.TransactionType;
+//import com.mindhub.homebanking.repositories.TransactionRepository;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+//
+//import java.util.List;
+//
+//import static org.hamcrest.MatcherAssert.assertThat;
+//import static org.hamcrest.Matchers.*;
+//@DataJpaTest
+//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+//public class TransactionRepositoryTest {
+//    @Autowired
+//    private TransactionRepository transactionRepository;
+//
+//    @Test
+//    public void existTransactionsByType(){
+//        List<Transaction> transactions = transactionRepository.findAll();
+//        assertThat(transactions, hasItem(hasProperty("type", is(TransactionType.CREDIT))));
+//    }
+//}
